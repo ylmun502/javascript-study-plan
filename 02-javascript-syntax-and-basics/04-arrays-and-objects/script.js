2331f4946d9b0f15acd0e1d3a434437a7c9b5961
Alan <ymun2@horizon.csueastbay.edu>
2025-10-31 22:20:32 -0700

    exercise(arrays-and-objects): add exercise for arrays and objects
    Declare an array and an object.

---
+++ 02-javascript-syntax-and-basics/04-arrays-and-objects/script.js
@@ -0,0 +1,13 @@
+const favoriteMovies = ["Interstellar", "Inception", 
+    "The Martian", "Star War", "Iron Man"];
+const movie = {
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    year: 2014
+}
+
+for(const movie of favoriteMovies) {
+    console.log(movie); 
+}
+
+console.log(movie);
