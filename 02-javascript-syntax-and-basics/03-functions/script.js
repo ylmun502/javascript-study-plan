6997aa81a882a6eb29e176df18d52b1fcd2fa0e0
Alan <ymun2@horizon.csueastbay.edu>
2025-10-31 21:46:03 -0700

    fix(functions): correct arguments when calling functions

--- 02-javascript-syntax-and-basics/03-functions/script.js
+++ 02-javascript-syntax-and-basics/03-functions/script.js
@@ -1,7 +1,7 @@
 function add(a, b) {
     return a + b;
 }
-console.log("Regular function:", add(10 + 8));
+console.log("Regular function:", add(10, 8));
 
 const addArrowFunction = (a, b) => a + b;
-console.log("Arrow function:", addArrowFunction(10 + 8));
+console.log("Arrow function:", addArrowFunction(10, 8));
